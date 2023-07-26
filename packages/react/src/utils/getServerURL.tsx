8de67eaae860e9e8659ddf756066111332a0ad3a
nbogie <neillbogie@googlemail.com>
2023-07-26 22:45:35 +0100

    have getServerURL consult REACT_APP_SERVER_URL

--- packages/react/src/utils/getServerURL.tsx
+++ packages/react/src/utils/getServerURL.tsx
@@ -1,11 +1,9 @@
 import { getEnvVarOrFail } from "./envVars";
 
 export function getServerURL() {
-    // const productionServerURL = getEnvVarOrFail("REACT_APP_SERVER_URL");
-    const localServerURL = "ws://localhost:2567";
-    // const url = productionServerURL;
+    const serverURL = getEnvVarOrFail("REACT_APP_SERVER_URL");
     // process.env.NODE_ENV === "production"
     //     ? productionServerURL
     //     : localServerURL;
-    return localServerURL;
+    return serverURL;
 }
